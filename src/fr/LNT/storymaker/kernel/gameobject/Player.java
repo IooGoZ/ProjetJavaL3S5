09856302d40d406596b10912d56224eecd0a407c
N0v444 <117517472+N0v444@users.noreply.github.com>
2022-11-17 10:08:15 +0100

    Update Player.java

--- src/fr/LNT/storymaker/kernel/gameobject/Player.java
+++ src/fr/LNT/storymaker/kernel/gameobject/Player.java
@@ -10,5 +10,25 @@ public class Player {
 	private List<Character> followers;
 	private int health;
 	private String name;
-
+	
+	public List<Item> getInventory()
+	{
+		return this.inventory;
+	}
+	
+	public void addInventory(Item item) {
+		this.inventory.add(item);
+	}
+	
+	public void imDying(int damage) {
+		this.health -= damage;
+	}
+	
+	public void move(Door door) {
+		if (door.whereAmI() == this.position) {
+			this.position = door.whereDoIGo();
+		}
+	}
+	
+	
 }
