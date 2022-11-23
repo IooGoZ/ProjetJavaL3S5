74ea1dc2c51b1403a3715d55941ccf62c8190a7c
L3n444 <118428403+L3n444@users.noreply.github.com>
2022-11-23 16:00:48 +0100

    Update Player.java

--- src/fr/LNT/storymaker/kernel/Player.java
+++ src/fr/LNT/storymaker/kernel/Player.java
@@ -11,25 +11,85 @@ public class Player {
 	private List<Item> inventory;
 	private List<Character> followers;
 	private int health;
-	private String name;
+	private final String name;
 	
-	public List<Item> getInventory()
-	{
-		return this.inventory;
+	
+	public Player(Location p, List<Item> i, List<Character> f, int h, String n){
+		this.position = p;
+		this.inventory = i;
+		this.followers = f;
+		this.health = h;
+		this.name = n;
 	}
 	
 	public void addInventory(Item item) {
 		this.inventory.add(item);
 	}
 	
-	public void imDying(int damage) {
+	public void removeIventory(Item item) {
+		this.inventory.remove(item);
+	}
+	
+	public void lostHealth(int damage) {
 		this.health -= damage;
 	}
 	
+	public void gainHealth (int point) {
+		this.health -= point;
+	}
+	
+	public void printStatePlayer() {
+		if ((this.health <= 100) && (this.health >= 80)) {
+			System.out.println("You are the best ! ");
+		} 
+		else if ((this.health < 80) && (this.health >= 60 )) {
+			System.out.println("You are doing great !");
+		}
+		else if (this.health == 50) {
+			System.out.println("You are not fine but not sad either");
+		}
+		else if ((this.health < 50) && (this.health >= 30)) {
+			System.out.println("You are trying your best, keep going");
+		}
+		else if ((this.health < 30) && (this.health < 10)) {
+			System.out.println("You are in depression");
+		}
+		else System.out.println("You are having a mental breakdown");
+	}
+	
+	public void death() {
+		if (this.health == 0) {
+			System.out.println("You are dead...");
+		}
+	}
+	
 	public void move(Door door) {
-		if (door.whereAmI() == this.position) {
+		if ((door.whereAmI() == this.position) && (door.isOpen())) {
 			this.position = door.whereDoIGo();
 		}
 	}
 	
+	
+	// ----- Getter ----- //
+	public List<Item> getInventory(){
+		return this.inventory;
+	}
+	
+	public int getHealth() {
+		return this.health;
+	}
+	
+	public Location getPosition() {
+		return this.position;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public List<Character> getFollowers(){
+		return this.followers;
+	}
+	
 }
+
