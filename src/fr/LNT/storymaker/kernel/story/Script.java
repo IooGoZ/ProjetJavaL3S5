f6a73f0511f08a6a56452153b50cc64ab7688c49
N0v444 <117517472+N0v444@users.noreply.github.com>
2022-11-23 16:05:23 +0100

    Update Script.java

--- src/fr/LNT/storymaker/kernel/story/Script.java
+++ src/fr/LNT/storymaker/kernel/story/Script.java
@@ -8,6 +8,15 @@ public class Script extends TreeObject {
 		super(starter_node);
 	}
 	
+	public Object[] getAllChildren() {
+		int len = super.current_node.getMaxChildrensIndex();
+		Object[] list = new Object[len];
+		for (int i = 0; i < len; i++)
+		{
+			list[i] = super.current_node.getChildren(i).getNodeValue();
+		}	
+		return list;
+	}
 	
 	
 }
