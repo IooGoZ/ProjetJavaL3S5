594d0e53ecfbf87035d63a77b8400ef68626d2ac
N0v444 <zelda.larouge@gmail.com>
2022-12-02 09:31:39 +0100

    Create Test.java

---
+++ src/fr/LNT/storymaker/debug/Test.java
@@ -0,0 +1,22 @@
+package fr.LNT.storymaker.debug;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class Test {
+    @Test
+    public void testInventory() {
+        //créer un location vide sauf un objet si possible
+        Item item = ;
+        CommandeTake.execute(item);
+        assertNull(location.items);
+    }
+
+    @Test
+    public void testOpenDoor() {
+        Door door = ; //porte fermée
+        
+    }
+
+    
+}
